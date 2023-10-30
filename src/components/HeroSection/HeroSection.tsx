7cf9244795f13aaa3855a38b181a126e6ffea774
Luis Martínez <lemartinezmendoza@gmail.com>
2023-10-30 13:36:01 -0500

    chore: add lora font and change background color in hero section

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -2,17 +2,20 @@ import "./styles.css";
 
 export default function HeroSection() {
   return (
-    <section className="hero-container">
-      <div className="left-color"></div>
-      <div className="right-color"></div>
-      <h1>Timeless Brews</h1>
-      <p>Roy Watts</p>
-      <p>First Tisa brewmaster</p>
-      <img
-        src="portrait-2-sm.png"
-        alt="Portrait founder"
-        className="portrait-image"
-      />
-    </section>
+    <>
+      <section className="hero-container">
+        <img
+          src="portrait-2-sm.png"
+          alt="Portrait founder"
+          className="portrait-image"
+        />
+
+        <div className="title-container">
+          <h1>Timeless Brews</h1>
+          <p>Robert Watson</p>
+          <p>First Tisa brewmaster</p>
+        </div>
+      </section>
+    </>
   );
 }
