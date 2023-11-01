7d86b4b988bb2e73d929970aca4563c092af692b
Luis Martínez <lemartinezmendoza@gmail.com>
2023-11-01 12:24:31 -0500

    feat: add description section

---
+++ src/components/DescriptionSection/DescriptionSection.tsx
@@ -0,0 +1,31 @@
+import "./styles.css";
+
+export default function DescriptionSection() {
+  return (
+    <section className="description-container">
+      <article className="description-left">
+        <h2 className="description-title">Forged from steel</h2>
+        <p className="description-subtitle">
+          Find out a few interesting facts about our signature steel timeless
+          can
+        </p>
+        <div className="image-container">
+          <img src="/timeless-1-nobg.png" alt="Timeless brews image" />
+        </div>
+      </article>
+      <article className="description-right">
+        <h2 className="description-title">Tisa's beer in America</h2>
+        <p className="description-subtitle">
+          Products sold in the U.S. are brewed in the U.S., Canada and Vietnam.
+        </p>
+        <img src="/logo-3-lg-light.png" alt="Logo" />
+        <p className="description-text">
+          Timeless brews premium lagers using the highest-quality ingredients we
+          cand find giving each of our beers our signature crisp, refreshing
+          flavor and clean finish. Whatever your taste, we've got a beer for
+          you.
+        </p>
+      </article>
+    </section>
+  );
+}
